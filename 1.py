d207b19a6535a98693a389a2d486040d921b5b10
Maks Iamchuk <maks.iamchuk@outlook.com>
2024-02-08 22:34:22 +0400

    ДЗ принято и защитано 12/12

--- 1.py
+++
@@ -1,6 +0,0 @@
-# Задание 1:
-name = input ('Введите имя:')
-surname = input ('Введите фамилию:')
-year = int (input('Введите год рождения:'))
-
-print (surname, name, ",", 2024 - year)
--- 3.py
+++
@@ -1,6 +0,0 @@
-# Задание 3:
-minute = int (input ('Введите минуты:'))
-
-print (
-    f"{minute} минут - это {int(minute/60)} ч {minute%60} мин"
-)
